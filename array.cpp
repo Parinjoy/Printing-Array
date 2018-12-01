0a8fc3d3bbea41dca7ea1953bd626c563520dc38
A Pramanik <35112273+Parinjoy@users.noreply.github.com>
2018-12-01 20:41:17 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main()
 {
+    //Declaring variables used here
     int n ;
     int numbers[n] ;
+    cout << "Enter size of array";
+    //User inputs Size Of The Array
     cin >> n ;
     cout << "Enter numbers: ";
+    //Making the Array
     for (int i = 0; i < n; ++i)
     {
         cin >> numbers[i];
     }
+    //Printing The Array
     for (int i = 0; i < n; i++)
 
     {cout << numbers[i];}
